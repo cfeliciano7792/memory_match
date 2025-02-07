69df82139a97f89ba37438a601de76f05bc944e8
cfeliciano <cfeliciano7792@gmail.com>
2025-02-06 21:09:02 -0500

    added collision between player and boxes

--- src/Scenes/game.js
+++ src/Scenes/game.js
@@ -1,72 +1,75 @@
 import Phaser from "phaser";
 
-export default class Game extends Phaser.Scene 
-{
-    constructor()
-    {
-        super('game')
-        this.cursors
-        this.player
-        this.speed = 200
-        this.boxGroup
-    }
+export default class Game extends Phaser.Scene {
+	constructor() {
+		super("game");
+		this.cursors;
+		this.player;
+		this.speed = 200;
+		this.boxGroup;
+	}
 
-    create()
-    {
-        const { width, height } = this.scale
-        this.player = this.physics.add.sprite(width * 0.5, height * 0.6, 'sokoban', 52).play('down-walk')
+	create() {
+		const { width, height } = this.scale;
+		this.player = this.physics.add
+			.sprite(width * 0.5, height * 0.6, "sokoban", 52)
+			.setSize(40, 16)
+			.setOffset(12, 38)
+			.play("down-idle");
 
-        this.cursors = this.input.keyboard.createCursorKeys()
+		this.cursors = this.input.keyboard.createCursorKeys();
 
-        // add boxes to the screen
-        this.boxGroup = this.physics.add.staticGroup()
+		// add boxes to the screen
+		this.boxGroup = this.physics.add.staticGroup();
 
-        let xPercent = 0.20
-        let yCor = 75
-        for (let row = 0; row < 4; ++row)
-        {
-            for (let col = 0; col < 4; ++col)
-            {
-                this.boxGroup.get(width * xPercent, yCor, 'sokoban', 10)
-                xPercent += 0.20
-            }
+		this.createBoxes();
 
-            xPercent = 0.20
-            yCor += 150
-        }
+		//collision between player and box
+		this.physics.add.collider(this.player, this.boxGroup);
+	}
 
-    }
+	update() {
+		if (this.cursors.left.isDown) {
+			this.player.setVelocity(-this.speed, 0);
+			this.player.play("left-walk", true);
+		} else if (this.cursors.right.isDown) {
+			this.player.setVelocity(this.speed, 0);
+			this.player.play("right-walk", true);
+		} else if (this.cursors.up.isDown) {
+			this.player.setVelocity(0, -this.speed);
+			this.player.play("up-walk", true);
+		} else if (this.cursors.down.isDown) {
+			this.player.setVelocity(0, this.speed);
+			this.player.play("down-walk", true);
+		} else {
+			this.player.setVelocity(0, 0);
+			// retrieve what direction the player is facing
+			const key = this.player.anims.currentAnim.key;
+			const parts = key.split("-");
+			const direction = parts[0];
+			// set idle animation to current direction
+			this.player.play(`${direction}-idle`);
+		}
 
-    update(){
-        if (this.cursors.left.isDown)
-        {
-            this.player.setVelocity(-this.speed, 0)
-            this.player.play('left-walk', true)
-        }
-        else if (this.cursors.right.isDown)
-        {
-            this.player.setVelocity(this.speed, 0)
-            this.player.play('right-walk', true)
-        }
-        else if (this.cursors.up.isDown)
-        {
-            this.player.setVelocity(0,-this.speed)
-            this.player.play('up-walk', true)
-        }
-        else if (this.cursors.down.isDown)
-        {
-            this.player.setVelocity(0, this.speed)
-            this.player.play('down-walk', true)
-        }
-        else
-        {
-            this.player.setVelocity(0, 0)
-            // retrieve what direction the player is facing
-            const key = this.player.anims.currentAnim.key
-            const parts = key.split('-')
-            const direction = parts[0]
-            // set idle animation to current direction
-            this.player.play(`${direction}-idle`)
-        }
-    }
+		// player depth
+		this.player.setDepth(this.player.y);
+	}
+
+	createBoxes() {
+		const { width, height } = this.scale;
+		let xPercent = 0.2;
+		let yCor = 75;
+		for (let row = 0; row < 4; ++row) {
+			for (let col = 0; col < 4; ++col) {
+				const box = this.boxGroup.create(width * xPercent, yCor, "sokoban", 6);
+				box.setSize(64, 32);
+				box.setOffset(0, 32);
+				box.setDepth(box.y);
+				xPercent += 0.2;
+			}
+
+			xPercent = 0.2;
+			yCor += 150;
+		}
+	}
 }
